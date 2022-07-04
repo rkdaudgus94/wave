34aa9c752055da5b4a6643bfd27354b34ce577bf
rkdaudgus94 <https://github.com/rkdaudgus94/auto_driving.git>
2022-07-04 17:34:18 +0900

    modify

--- ultrasonicwave.py
+++ ultrasonicwave.py
@@ -25,14 +25,14 @@ try :
 
         while GPIO.input(ECHO) == 0:
             start = time.time()
-            print(start)
+            print("start" + start)
         
         while GPIO.input(ECHO) == 1:
             end = time.time()
-            print(end)
+            print("end" + end)
 
         check_time = end - start
-        print(check_time)
+        print("check_time" + check_time)
         distance = check_time * 17000
         print("Distance = %.1f cm" %distance)
         time.sleep(0.4)
