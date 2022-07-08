65a46d74adfd0d939c1c7c5ddeb8923185ddbb5c
rkdaudgus94 <https://github.com/rkdaudgus94/auto_driving.git>
2022-07-08 13:18:18 +0900

    2

--- simple_plot.py
+++ simple_plot.py
@@ -11,7 +11,7 @@ import numpy as np
 
 # Data for plotting
 t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
+s = np.sin(2 * np.pi * t)
 
 fig, ax = plt.subplots()
 ax.plot(t, s)
--- ultrasonicwave.py
+++ ultrasonicwave.py
@@ -1,5 +1,8 @@
 import RPi.GPIO as GPIO
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+
 # test .
 GPIO.setmode(GPIO.BCM) # (BCM 명령어를 사용하면 GPIO 넘버) : (BOARD 명령어를 사용하면 핀 넘버) 
 GPIO.setwarnings(False)
@@ -15,10 +18,13 @@ GPIO.output(TRIG, False)
 print ("output initialization")
 time.sleep(1)
 
+d = []
+
 try :
     start = 0
     end = 0
-    while True:
+    sec = 0
+    while sec <= 10:
         GPIO.output(TRIG,True)
         time.sleep(0.00001)
         GPIO.output(TRIG,False)
@@ -32,8 +38,23 @@ try :
         check_time = end - start
         distance = check_time * 17000
         print("Distance = %.1f cm" %distance)
-        time.sleep(0.4)
+        time.sleep(1)
+        d.append(distance)
+        sec = sec + 1
 
 except KeyboardInterrupt:
     print("Complete mesuring")
     GPIO.cleanup()
+
+# t  = np.arange(0,10,1)
+# d1 = np.d
+
+# fig, ax = plt.subplot()
+# ax.plot(t,d1)
+
+# ax.set(xlabel = 'time (s)', ylabel = 'distance (cm)',
+# title = 'Ultrasonic wave distance mesure')
+# ax.grid()
+
+# fig.savefig("test2,png")
+# plt.show()
