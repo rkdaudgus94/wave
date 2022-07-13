2daec6dfa52928e5388752316fb538637b9d21a1
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2022-07-13 16:45:23 +0900

    Update import serial.py

--- import serial.py
+++ import serial.py
@@ -2,18 +2,22 @@ import serial
 port = '/dev/ttyACM0'
 brate = 115200
 ser = serial.Serial(port, brate)
-while True :
-    ch = input()
-    if ch == 'stop' :
-        ser.write('stop'.encode())
-        data = ser.readline()
-        data = data.decode()[:len(data)-2]
-        print(len(data))
-        print(data)
-        
-    if ch == 'front' :
-        ser.write('front'.encode())  
-        data = ser.readline()
-        data = data.decode()[:len(data)-2]
-        print(len(data))
-        print(data)
+try:
+    while True :
+        ch = input()
+        if ch == 'stop' :
+            ser.write(ch.encode())
+            data = ser.readline()
+            data = data.decode()[:len(data)-2]
+            print(len(data))
+            print(data)
+            
+        if ch == 'front' :
+            ser.write(ch.encode())  
+            data = ser.readline()
+            data = data.decode()[:len(data)-2]
+            print(len(data))
+            print(data)
+
+except KeyboardInterrupt :
+    pass
