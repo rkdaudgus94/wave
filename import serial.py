44d6df4110989903de1cda16a9d035d50f29cdd3
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2022-07-13 16:20:39 +0900

    4

--- import serial.py
+++ import serial.py
@@ -2,18 +2,18 @@ import serial
 # s
 
 port = '/dev/ttyACM0'
-brate = 9600
+brate = 115200
 ser = serial.Serial(port, brate, timeout=None)
 while True :
     ch = input()
-    if ch == 's' :
+    if ch == 'stop' :
         ser.write(ch.encode())
         data = ser.readline()
         data = data.decode()[:len(data)-2]
         print(len(data))
         print(data)
         
-    if ch == 'f' :
+    if ch == 'foward' :
         ser.write(ch.encode())  
         data = ser.readline()
         data = data.decode()[:len(data)-2]
