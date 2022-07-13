1e758d3aeae26098c9db37b2c97b63d1a3965bf1
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2022-07-13 16:19:50 +0900

    4

---
+++ import serial.py
@@ -0,0 +1,21 @@
+import serial
+# s
+
+port = '/dev/ttyACM0'
+brate = 9600
+ser = serial.Serial(port, brate, timeout=None)
+while True :
+    ch = input()
+    if ch == 's' :
+        ser.write(ch.encode())
+        data = ser.readline()
+        data = data.decode()[:len(data)-2]
+        print(len(data))
+        print(data)
+        
+    if ch == 'f' :
+        ser.write(ch.encode())  
+        data = ser.readline()
+        data = data.decode()[:len(data)-2]
+        print(len(data))
+        print(data)
